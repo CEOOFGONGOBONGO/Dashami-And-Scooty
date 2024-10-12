717e83a941bd3d52e0d6c060eae88773a49e9ffe
Nobita nobi <163984047+Somu6969@users.noreply.github.com>
2024-10-12 20:24:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const username = 'Somu6969' //write your server github username 
-const reponame = 'Dashami-and-Scooty'             // Write your server github repo name
+const reponame = 'Dashami-Scooty'  // Write your server github repo name
 const repoURL = `https://api.github.com/repos/${username}/${reponame}/contents/`; // Replace with your GitHub repo details
 
 //Media Data Types
@@ -9,53 +9,48 @@ const mediaTypes = {
 }
 
 async function fetchMedia() {
-    // Get the token from the password input
-    const token = document.querySelector("#password").value;
     try {
         const response = await fetch(repoURL);
         const data = await response.json();
+        const videoGalleryContent = document.querySelector(".video-gallery .contents");
+        const imageGalleryContent = document.querySelector(".image-gallery .contents");
 
-         
-            document.querySelector("html").innerHTML = insta_sexy_html;
-            const videoGalleryContent = document.querySelector(".video-gallery .contents");
-            const imageGalleryContent = document.querySelector(".image-gallery .contents");
-
-            // Render Images and Videos
-            data.forEach(file => {
-                mediaTypes.pictures.forEach(element => {
-                    if (file.name.endsWith(element)) {
-                        const imagecard = document.createElement("div");
-                        imagecard.setAttribute("class", "image-card");
-                        imageGalleryContent.appendChild(imagecard);
-                        const img = document.createElement('img');
-                        const a = document.createElement('a');
-                        const baseName = file.name.substring(0, file.name.lastIndexOf('.'));  // Extracts up to the last dot
-                        a.setAttribute("id", `${baseName}`);
-                        // a.href=file.download_url
-                        img.src = file.download_url;
-                        img.alt = file.name;
-                        imagecard.appendChild(a);
-                        a.appendChild(img);
-                    }
-                });
-
-                mediaTypes.videos.forEach(element => {
-                    if (file.name.endsWith(element)) {
-                        const videocard = document.createElement("div");
-                        videocard.setAttribute("class", "video-card");
-                        videoGalleryContent.appendChild(videocard);
-                        const video = document.createElement('video');
-                        video.controls = true;
-                        video.src = file.download_url;
-                        videocard.appendChild(video);
-                    }
-                });
+        // Render Images and Videos
+        data.forEach(file => {
+            mediaTypes.pictures.forEach(element => {
+                if (file.name.endsWith(element)) {
+                    const imagecard = document.createElement("div");
+                    imagecard.setAttribute("class", "image-card");
+                    imageGalleryContent.appendChild(imagecard);
+                    const img = document.createElement('img');
+                    const a = document.createElement('a');
+                    const baseName = file.name.substring(0, file.name.lastIndexOf('.'));  // Extracts up to the last dot
+                    a.setAttribute("id", `${baseName}`);
+                    // a.href=file.download_url
+                    img.src = file.download_url;
+                    img.alt = file.name;
+                    imagecard.appendChild(a);
+                    a.appendChild(img);
+                }
             });
 
-            // Call function to set ARW links after media is rendered
-            setARWLinks(data)
+            mediaTypes.videos.forEach(element => {
+                if (file.name.endsWith(element)) {
+                    const videocard = document.createElement("div");
+                    videocard.setAttribute("class", "video-card");
+                    videoGalleryContent.appendChild(videocard);
+                    const video = document.createElement('video');
+                    video.controls = true;
+                    video.src = file.download_url;
+                    videocard.appendChild(video);
+                }
+            });
+        });
 
-        
+        // Call function to set ARW links after media is rendered
+        setARWLinks(data)
+
+
 
     } catch (error) {
         console.error('Error fetching media:', error);
@@ -65,12 +60,14 @@ async function fetchMedia() {
 // Function to set ARW file links after rendering other media
 function setARWLinks(data) {
     data.forEach(file => {
-        if (file.name.endsWith(".ARW")|| file.name.endsWith(".dng")) {
+        if (file.name.endsWith(".ARW") || file.name.endsWith(".dng")) {
             var target_a = document.getElementById(`${file.name}`);
             if (target_a) {
                 target_a.href = file.download_url; // Set the href only if the element exists
-                // target_a.download=`${file.name}`
+                target_a.download=`${file.name}`
             }
         }
     });
 }
+
+fetchMedia()
